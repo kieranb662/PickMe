05c3c8e330d58a56312c9b4b58f7894aa2cc6c58
Kieran Brown <kb6627500@gmail.com>
2020-03-28 13:48:05 -0400

    Initial Commit

---
+++ Sources/PickMe/PickMe.swift
@@ -0,0 +1,3 @@
+struct PickMe {
+    var text = "Hello, World!"
+}
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import PickMeTests
+
+var tests = [XCTestCaseEntry]()
+tests += PickMeTests.allTests()
+XCTMain(tests)
